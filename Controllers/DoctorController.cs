366962e6ba27ffb3f52eb9e7cf6345bcc48da31f
hodhod212 <hodhod212@yahoo.com>
2021-11-12 10:43:47 +0100

    Created Doctor model

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Utility;
+
+namespace WebApplication1.Controllers
+{
+    public class DoctorController : Controller
+    {      
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult FeberCheck()
+        {
+            return View();
+        }
+        TempResult tempResult = new TempResult();
+        [HttpPost]
+        public IActionResult Index(DoctorModel temprature)
+        {           
+
+            ViewBag.TempResult = tempResult.GetResult(ref temprature);
+            return View();
+        }
+    }
+}
---
+++ Models/TempResult.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Utility
+{
+    public class TempResult
+    {
+      
+        public string GetResult(ref DoctorModel _myModel)
+        {
+
+            try
+            {
+                string message = "";
+                if (_myModel.temperature > 38)
+                {
+
+                    message = $"You have feber, because your body temprature is {_myModel.temperature} °C";
+                }
+                else if (_myModel.temperature < 35)
+                {
+
+                    message = $"You have hypotermi, because your body temprature is {_myModel.temperature} °C";
+                }
+                else
+                {
+
+                    message = $"It is ok, because your body temprature is {_myModel.temperature} °C";
+                }
+                
+                return message;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
